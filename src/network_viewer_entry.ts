db88f66c259e3205c4643e7f2e232732bb505d86
anjali <anjali@ebi.ac.uk>
2019-08-22 16:00:39 +0100

    NIV-326 : cytoscape initialization written

--- src/network_viewer_entry.ts
+++ src/network_viewer_entry.ts
@@ -1,32 +1,30 @@
 import cytoscape from 'cytoscape';
-
-/*
-export class NetworkViewer {
-    //field
-    engine:string;
-
-    //constructor
-    constructor(engine:string) {
-        this.engine = engine
-    }
-
-    //function
-    disp():void {
-        console.log("Engine is  :   "+this.engine)
-    }
-}*/
+import $ from "jquery";
 
 export class InitializeGraph {
     //field
-    engine:string;
+    graph_container_div_id:string;
+    cy: any;
 
     //constructor
-    constructor(engine:string) {
-        this.engine = engine
+    constructor(graph_container_div_id:string) {
+        this.graph_container_div_id = graph_container_div_id;
+        this.initializeCytoscape();
     }
 
     //function
     disp():void {
-        console.log("Engine is  :   "+this.engine)
+        console.log("Engine is  :   "+this.graph_container_div_id)
     }
+
+    //function
+    initializeCytoscape():void {
+        this.cy = cytoscape({
+            container: $('#'+this.graph_container_div_id)
+        });
+        console.log("cy is"+this.cy);
+
+    }
+
+
 }
