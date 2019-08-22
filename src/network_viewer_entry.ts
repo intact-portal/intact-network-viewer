e06d8e6ac28a60885e18ff2eeb173ba1535e478b
anjali <anjali@ebi.ac.uk>
2019-08-22 16:40:10 +0100

    NIV-326 : cytoscape example fcose layout

--- src/network_viewer_entry.ts
+++ src/network_viewer_entry.ts
@@ -1,5 +1,6 @@
 import cytoscape from 'cytoscape';
 import fcose from 'cytoscape-fcose';
+cytoscape.use( fcose );
 import $ from "jquery";
 
 export class InitializeGraph {
@@ -22,7 +23,116 @@ export class InitializeGraph {
     initializeCytoscape():void {
         this.cy = cytoscape({
             container: $('#'+this.graph_container_div_id)
+            , // container to render in
+
+            elements: [ // list of graph elements to start with
+            { // node a
+                data: { id: 'a' }
+            },
+            { // node b
+                data: { id: 'b' }
+            },
+            { // edge ab
+                data: { id: 'ab', source: 'a', target: 'b' }
+            }
+        ],
+
+            style: [ // the stylesheet for the graph
+            {
+                selector: 'node',
+                style: {
+                    'background-color': '#666',
+                    'label': 'data(id)'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'width': 3,
+                    'line-color': '#ccc',
+                    'target-arrow-color': '#ccc',
+                    'target-arrow-shape': 'triangle'
+                }
+            }
+        ],
+
+            layout: {
+            name: 'grid',
+                rows: 1
+        }
         });
+
+        var defaultOptions = {
+
+            // 'draft', 'default' or 'proof'
+            // - "draft" only applies spectral layout
+            // - "default" improves the quality with incremental layout (fast cooling rate)
+            // - "proof" improves the quality with incremental layout (slow cooling rate)
+            quality: "default",
+            // use random node positions at beginning of layout
+            // if this is set to false, then quality option must be "proof"
+            randomize: true,
+            // whether or not to animate the layout
+            animate: true,
+            // duration of animation in ms, if enabled
+            animationDuration: 1000,
+            // easing of animation, if enabled
+            animationEasing: undefined,
+            // fit the viewport to the repositioned nodes
+            fit: true,
+            // padding around layout
+            padding: 10,
+            // whether to include labels in node dimensions. Valid in "proof" quality
+            nodeDimensionsIncludeLabels: false,
+
+            /* spectral layout options */
+
+            // false for random, true for greedy sampling
+            samplingType: true,
+            // sample size to construct distance matrix
+            sampleSize: 25,
+            // separation amount between nodes
+            nodeSeparation: 75,
+            // power iteration tolerance
+            piTol: 0.0000001,
+
+            /* incremental layout options */
+
+            // Node repulsion (non overlapping) multiplier
+            nodeRepulsion: 4500,
+            // Ideal edge (non nested) length
+            idealEdgeLength: 50,
+            // Divisor to compute edge forces
+            edgeElasticity: 0.45,
+            // Nesting factor (multiplier) to compute ideal edge length for nested edges
+            nestingFactor: 0.1,
+            // Gravity force (constant)
+            gravity: 0.25,
+            // Maximum number of iterations to perform
+            numIter: 2500,
+            // For enabling tiling
+            tile: false,
+            // Represents the amount of the vertical space to put between the zero degree members during the tiling operation(can also be a function)
+            tilingPaddingVertical: 10,
+            // Represents the amount of the horizontal space to put between the zero degree members during the tiling operation(can also be a function)
+            tilingPaddingHorizontal: 10,
+            // Gravity range (constant) for compounds
+            gravityRangeCompound: 1.5,
+            // Gravity force (constant) for compounds
+            gravityCompound: 1.0,
+            // Gravity range (constant)
+            gravityRange: 3.8,
+            // Initial cooling factor for incremental layout
+            initialEnergyOnIncremental: 0.3,
+
+            /* layout event callbacks */
+            ready: () => {}, // on layoutready
+            stop: () => {} // on layoutstop
+        };
+
+        var layout =this.cy.layout({ name: 'fcose', defaultOptions }).run();
+
         console.log("cy is"+this.cy);
 
     }
