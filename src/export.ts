13cbf1a25b4bbc7e418b6c78dbb6954b537ae3b1
anjali <anjali@ebi.ac.uk>
2019-11-15 15:25:44 +0000

    NIV-25:8th requirement details: graphml download : finished

--- src/export.ts
+++ src/export.ts
@@ -6,7 +6,17 @@ export class Export {
   }
 
   public exportAsGraphml(): void {
-    alert(this.cy);
-    this.cy.graphml();
+    console.log(this.cy.graphml());
+    let element = document.createElement('a');
+    let filename="network.graphml";
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(this.cy.graphml()));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
   }
 }
