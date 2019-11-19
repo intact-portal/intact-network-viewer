fb1006c5704b835ed2ff9ef6f6946cb6ecb1df25
anjali <anjali@ebi.ac.uk>
2019-11-19 16:14:16 +0000

    NIV-25 : Removed linting errors in compound_node_color_legend.ts

--- src/legends/nodes/compound_node_color_legend.ts
+++ src/legends/nodes/compound_node_color_legend.ts
@@ -3,7 +3,7 @@ import { Style } from './../style';
 import { Utility } from './../utility';
 
 export class CompoundNodeColorLegend {
-  private colors: Array<string>;
+  private colors: string[];
   private utility: Utility;
 
   private META_NODE_IMG_URL = require('./images/node-colors/meta-node.svg');
@@ -15,25 +15,25 @@ export class CompoundNodeColorLegend {
   }
 
   public createLegend(): HTMLDivElement {
-    let legendDiv = this.utility.createLegendDivFor(
+    const legendDiv = this.utility.createLegendDivFor(
       Constants.COMPOUND_NODE_COLOR_LEGEND_DIV_ID,
-      Constants.COMPOUND_NODE_COLOR_LEGEND_TITLE,
+      Constants.COMPOUND_NODE_COLOR_LEGEND_TITLE
     );
 
     let isSingleElement: boolean = false;
-    if (this.colors.length == 1) {
+    if (this.colors.length === 1) {
       isSingleElement = true;
     }
 
     this.colors.forEach(color => {
-      let trimmedColor = color.replace(/\s/g, '');
+      const trimmedColor = color.replace(/\s/g, '');
       switch (trimmedColor) {
         case Constants.META_NODE_COLOR: {
-          let divListElement = this.utility.createDivElementFor(
+          const divListElement = this.utility.createDivElementFor(
             this.META_NODE_IMG_URL,
             Constants.META_NODE_COLOR_LABEL,
             isSingleElement,
-            false,
+            false
           );
           legendDiv.appendChild(divListElement);
           break;
