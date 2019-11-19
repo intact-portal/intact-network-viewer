9036d5ca4855ba31fd4eb2f0bb9ff0ed0ba3f945
anjali <anjali@ebi.ac.uk>
2019-11-19 16:09:24 +0000

    NIV-25 : Removed linting errors in edge_thickness_legend.ts

--- src/legends/edges/edge_thickness_legend.ts
+++ src/legends/edges/edge_thickness_legend.ts
@@ -1,11 +1,11 @@
+import { NetworkViewerStates } from '../../network_viewer_states';
+import { Width } from '../../styles/constants/width';
 import { Constants } from './../constants';
 import { Style } from './../style';
 import { Utility } from './../utility';
-import { NetworkViewerStates } from '../../network_viewer_states';
-import { Width } from '../../styles/constants/width';
 
 export class EdgeThicknessLegend {
-  private thickness: Array<string>;
+  private thickness: string[];
   private utility: Utility;
 
   private SINGLE_INTERACTION_COLLAPSED_IMG_URL = require('./images/edge-thickness/continious-line.svg');
@@ -17,34 +17,34 @@ export class EdgeThicknessLegend {
   }
 
   public createLegend(): HTMLDivElement {
-    let legendDiv = this.utility.createLegendDivFor(
+    const legendDiv = this.utility.createLegendDivFor(
       Constants.EDGE_THICKNESS_LEGEND_DIV_ID,
-      Constants.EDGE_THICKNESS_LEGEND_TITLE,
+      Constants.EDGE_THICKNESS_LEGEND_TITLE
     );
 
     let isSingleElement: boolean = false;
-    if (this.thickness.length == 1) {
+    if (this.thickness.length === 1) {
       isSingleElement = true;
     }
 
     this.thickness.forEach(thickness => {
       switch (thickness) {
         case Width.COLLAPSED_EDGE: {
-          let divListElement = this.utility.createDivElementFor(
+          const divListElement = this.utility.createDivElementFor(
             this.MULT_INTERACTION_COLLAPSED_IMG_URL,
             Constants.EDGE_MULT_INTERACTION_COLLAPSED_LABEL,
             isSingleElement,
-            true,
+            true
           );
           legendDiv.appendChild(divListElement);
           break;
         }
         case Width.DEFAULT_EDGE: {
-          let divListElement = this.utility.createDivElementFor(
+          const divListElement = this.utility.createDivElementFor(
             this.SINGLE_INTERACTION_COLLAPSED_IMG_URL,
             Constants.EDGE_SINGLE_INTERACTION_COLLAPSED_LABEL,
             isSingleElement,
-            true,
+            true
           );
           legendDiv.appendChild(divListElement);
           break;
