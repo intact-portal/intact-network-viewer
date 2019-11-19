a83b92293d49b969bd061f9d35275b800963603e
anjali <anjali@ebi.ac.uk>
2019-11-19 15:17:20 +0000

    NIV-25 : Removed linting errors in fcose,ngraph layout.ts

--- src/layouts/ngraph_layout.ts
+++ src/layouts/ngraph_layout.ts
@@ -1,10 +1,8 @@
-import { Constants } from './constants';
 import { Global } from './../global';
+import { Constants } from './constants';
 
 export class NgraphLayout {
   private options: any = Constants.NGRAPH_LAYOUT_OPTIONS;
-
-  constructor() {}
 
   public execute(): void {
     const localOptions = this.options; // don't know why but we have to do this, can't access class variable directly
