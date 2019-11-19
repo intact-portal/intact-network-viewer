2392035140f8c8ab8a28456115082ff6e58a61f3
anjali <anjali@ebi.ac.uk>
2019-11-19 15:44:01 +0000

    NIV-25 : Removed linting errors in edge_color_legend.ts

--- src/legends/edges/edge_color_legend.ts
+++ src/legends/edges/edge_color_legend.ts
@@ -1,10 +1,10 @@
+import { NetworkViewerStates } from '../../network_viewer_states';
 import { Constants } from './../constants';
 import { Style } from './../style';
 import { Utility } from './../utility';
-import { NetworkViewerStates } from '../../network_viewer_states';
 
 export class EdgeColorLegend {
-  private colors: Array<string>;
+  private colors: string[];
   private utility: Utility;
 
   private PHYSICAL_ASSOCIATION_IMG_URL = require('./images/edge-colors/physical-association.svg');
@@ -22,118 +22,99 @@ export class EdgeColorLegend {
     this.colors = colors;
   }
 
-  private createMiScoreDivElement(elementImage: string): HTMLDivElement {
-    let miscoreLegendDivElement = <HTMLDivElement>document.createElement('div');
-
-    miscoreLegendDivElement.setAttribute('style', Style.MISCORE_SUB_DIV);
-
-    let miScoreLegendImage = <HTMLImageElement>document.createElement('img');
-    miScoreLegendImage.setAttribute('src', elementImage);
-
-    miScoreLegendImage.setAttribute('style', Style.MISCORE_GRADIENT_IMG);
-    miscoreLegendDivElement.appendChild(miScoreLegendImage);
-
-    let miScoreLegendImageLabel = <HTMLParagraphElement>document.createElement('p');
-    miScoreLegendImageLabel.setAttribute('style', Style.MISCORE_TEXT);
-    miScoreLegendImageLabel.innerHTML = Constants.MI_SCORE_LABEL;
-    miscoreLegendDivElement.appendChild(miScoreLegendImageLabel);
-
-    return miscoreLegendDivElement;
-  }
-
   public createLegend(graphState: string): HTMLDivElement {
-    let legendDiv = this.utility.createLegendDivFor(
-      Constants.EDGE_COLOR_LEGEND_DIV_ID,
-      Constants.EDGE_COLOR_LEGEND_TITLE,
+    const legendDiv = this.utility.createLegendDivFor(
+        Constants.EDGE_COLOR_LEGEND_DIV_ID,
+        Constants.EDGE_COLOR_LEGEND_TITLE
     );
 
-    if (graphState == NetworkViewerStates.COLLAPSED) {
+    if (graphState === NetworkViewerStates.COLLAPSED) {
       legendDiv.appendChild(this.createMiScoreDivElement(this.MI_SCORE_IMG_URL));
     } else {
       let isSingleElement: boolean = false;
-      if (this.colors.length == 1) {
+      if (this.colors.length === 1) {
         isSingleElement = true;
       }
 
       this.colors.forEach(color => {
-        let trimmedColor = color.replace(/\s/g, '');
+        const trimmedColor = color.replace(/\s/g, '');
         switch (trimmedColor) {
           case Constants.EDGE_COLOR_PHYSICAL_ASSOCIATION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.PHYSICAL_ASSOCIATION_IMG_URL,
-              Constants.EDGE_COLOR_PHYSICAL_ASSOCIATION_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.PHYSICAL_ASSOCIATION_IMG_URL,
+                Constants.EDGE_COLOR_PHYSICAL_ASSOCIATION_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_ASSOCIATION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.ASSOCIATION_IMG_URL,
-              Constants.EDGE_COLOR_ASSOCIATION_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.ASSOCIATION_IMG_URL,
+                Constants.EDGE_COLOR_ASSOCIATION_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_COLOCALIZATION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.COLOCALIZATION_IMG_URL,
-              Constants.EDGE_COLOR_COLOCALIZATION_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.COLOCALIZATION_IMG_URL,
+                Constants.EDGE_COLOR_COLOCALIZATION_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_DEPHOSPHORYLATION_REACTION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.DEPHOSPHORYLATION_REACTION_IMG_URL,
-              Constants.EDGE_COLOR_DEPHOSPHORYLATION_REACTION_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.DEPHOSPHORYLATION_REACTION_IMG_URL,
+                Constants.EDGE_COLOR_DEPHOSPHORYLATION_REACTION_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_DIRECT_INTERACTION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.DIRECT_INTERACTION_IMG_URL,
-              Constants.EDGE_COLOR_DIRECT_INTERACTION_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.DIRECT_INTERACTION_IMG_URL,
+                Constants.EDGE_COLOR_DIRECT_INTERACTION_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_MUTATION_IN_INTERACTION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.MUTATION_IN_INTERACTION_IMG_URL,
-              Constants.EDGE_COLOR_MUTATION_IN_INTERACTION_LABEL,
-              true,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.MUTATION_IN_INTERACTION_IMG_URL,
+                Constants.EDGE_COLOR_MUTATION_IN_INTERACTION_LABEL,
+                true,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_PHOSPHORYLATION_REACTION: {
-            let divListElement = this.utility.createDivElementFor(
-              this.PHOSPHORYLATION_REACTION_IMG_URL,
-              Constants.EDGE_COLOR_PHOSPHORYLATION_REACTION_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.PHOSPHORYLATION_REACTION_IMG_URL,
+                Constants.EDGE_COLOR_PHOSPHORYLATION_REACTION_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
           }
           case Constants.EDGE_COLOR_OTHERS: {
-            let divListElement = this.utility.createDivElementFor(
-              this.OTHERS_IMG_URL,
-              Constants.EDGE_COLOR_OTHERS_LABEL,
-              isSingleElement,
-              true,
+            const divListElement = this.utility.createDivElementFor(
+                this.OTHERS_IMG_URL,
+                Constants.EDGE_COLOR_OTHERS_LABEL,
+                isSingleElement,
+                true
             );
             legendDiv.appendChild(divListElement);
             break;
@@ -147,4 +128,25 @@ export class EdgeColorLegend {
 
     return legendDiv;
   }
+
+  private createMiScoreDivElement(elementImage: string): HTMLDivElement {
+    const miscoreLegendDivElement = document.createElement('div') as HTMLDivElement;
+
+    miscoreLegendDivElement.setAttribute('style', Style.MISCORE_SUB_DIV);
+
+    const miScoreLegendImage = document.createElement('img') as HTMLImageElement;
+    miScoreLegendImage.setAttribute('src', elementImage);
+
+    miScoreLegendImage.setAttribute('style', Style.MISCORE_GRADIENT_IMG);
+    miscoreLegendDivElement.appendChild(miScoreLegendImage);
+
+    const miScoreLegendImageLabel = document.createElement('p') as HTMLParagraphElement;
+    miScoreLegendImageLabel.setAttribute('style', Style.MISCORE_TEXT);
+    miScoreLegendImageLabel.innerHTML = Constants.MI_SCORE_LABEL;
+    miscoreLegendDivElement.appendChild(miScoreLegendImageLabel);
+
+    return miscoreLegendDivElement;
+  }
+
+
 }
