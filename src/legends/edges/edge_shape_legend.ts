9a879d99dc0d8080f3b57836d08e78dfe6b835ed
anjali <anjali@ebi.ac.uk>
2019-11-19 16:04:32 +0000

    NIV-25 : Removed linting errors in edge_shape_legend.ts

--- src/legends/edges/edge_shape_legend.ts
+++ src/legends/edges/edge_shape_legend.ts
@@ -1,12 +1,12 @@
+import { NetworkViewerStates } from '../../network_viewer_states';
+import { Shape } from '../../styles/constants/shape';
+import { Width } from '../../styles/constants/width';
 import { Constants } from './../constants';
 import { Style } from './../style';
 import { Utility } from './../utility';
-import { NetworkViewerStates } from '../../network_viewer_states';
-import { Width } from '../../styles/constants/width';
-import { Shape } from '../../styles/constants/shape';
 
 export class EdgeShapesLegend {
-  private shapes: Array<string>;
+  private shapes: string[];
   private utility: Utility;
 
   private SPOKE_EXPANDED_IMG_URL = require('./images/edge-shapes/dashed-line.svg');
@@ -17,24 +17,24 @@ export class EdgeShapesLegend {
   }
 
   public createLegend(): HTMLDivElement {
-    let legendDiv = this.utility.createLegendDivFor(
+    const legendDiv = this.utility.createLegendDivFor(
       Constants.EDGE_SHAPES_LEGEND_DIV_ID,
-      Constants.EDGE_SHAPES_LEGEND_TITLE,
+      Constants.EDGE_SHAPES_LEGEND_TITLE
     );
 
     let isSingleElement: boolean = false;
-    if (this.shapes.length == 1) {
+    if (this.shapes.length === 1) {
       isSingleElement = true;
     }
 
     this.shapes.forEach(shapes => {
       switch (shapes) {
         case Constants.EDGE_SHAPE_SPOKE_EXPANDED: {
-          let divListElement = this.utility.createDivElementFor(
+          const divListElement = this.utility.createDivElementFor(
             this.SPOKE_EXPANDED_IMG_URL,
             Constants.EDGE_SHAPE_SPOKE_EXPANDED_LABEL,
             true,
-            true,
+            true
           );
           legendDiv.appendChild(divListElement);
           break;
