e994e779949d6e8d21dd98962d295835629a4a24
EliotRagueneau <orkeliot@gmail.com>
2021-11-05 21:06:56 +0000

    :arrow_up: Upgrade webpack to optimise bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,51 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/network_viewer_entry.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.tsx?$/,
-                use: [{
-                    loader: 'expose-loader',
-                    options: 'IntactGraph'
-                }, {
-                    loader: 'ts-loader'
-                }]
-
+  mode: 'production',
+  entry: {
+    'IntActNetworkViewer': path.resolve('src/index.ts'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          // css-loader
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
             },
-            {
-                include: [/node_modules/],
-                test: /\.css$/,
-                use: [
-                { loader: 'style-loader' },
-                // css-loader
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: false
-                    }
-                }
-            ] },
-            {
-                test: /\.(png|svg|jpg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js','.css','.svg','jpg' ]
-    },
-    output: {
-        filename: 'intact_network.js',
-        library: 'IntactGraph',
-        libraryTarget: 'commonjs2',
-        path: path.resolve(__dirname, 'build')
-    }
-    /*output: {
-        filename: 'intact_network_for_html.js',
-        library: 'IntactGraph',
-        path: path.resolve(__dirname, 'build')
-    }*/,
-    optimization: {
-    minimize: false
-},
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css', '.svg', 'jpg'],
+  },
+  output: {
+    filename: 'intact-network-viewer.js',
+    library: 'IntActNetworkViewer',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'build'),
+  }
 };
