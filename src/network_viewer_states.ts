24b6419573ceff36f177d0df2db909169ea5dd94
EliotRagueneau <159753.AZErty>
2021-11-16 14:55:31 +0000

    :wastebasket: Remove unused file

--- src/network_viewer_states.ts
+++
@@ -1,5 +0,0 @@
-export class NetworkViewerStates {
-  public static EXPANDED: string = 'expanded';
-  public static COLLAPSED: string = 'collapsed';
-  public static MUTATION_EFFECTED: string = 'mutation effected';
-}
